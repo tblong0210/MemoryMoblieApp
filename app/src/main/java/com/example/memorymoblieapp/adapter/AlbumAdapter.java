f387e0ea647054a42beaa857490ec71619e39164
Phuoc Dinh <phuocdinh462@gmail.com>
2023-03-13 01:54:35 +0700

    Update AlbumAdapter.java

--- app/src/main/java/com/example/memorymoblieapp/adapter/AlbumAdapter.java
+++ app/src/main/java/com/example/memorymoblieapp/adapter/AlbumAdapter.java
@@ -1,12 +1,10 @@
 package com.example.memorymoblieapp.adapter;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
